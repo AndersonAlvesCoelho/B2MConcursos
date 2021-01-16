d6ce0c58fc5b4d566873c7bd9c183f7c4bcce038
jaolima <jaolima24@gmail.com>
2021-01-16 03:05:35 -0300

    make migration question

--- back-end/migrations/20210116025759_question.js
+++ back-end/migrations/20210116025759_question.js
@@ -3,6 +3,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('question', function(table){
         table.increments('id_question')
         table.string('name_comment_answer', 50).unique().notNullable()
+        table.string('year')
+        table.string('issue_resolution')
 
         table.integer('id_bank').unsigned()
         table.foreign('id_bank').references('bank.id_bank')
@@ -15,6 +17,12 @@ exports.up = function(knex) {
 
         table.integer('id_dicipline').unsigned()
         table.foreign('id_dicipline').references('dicipline.id_dicipline')
+
+        table.integer('id_user').unsigned()
+        table.foreign('id_user').references('user.id_user')
+
+        table.integer('id_comment').unsigned()
+        table.foreign('id_comment').references('comment.id_comment')
 
         table.timestamp('created_at').defaultTo(knex.fn.now())
         table.timestamp('updated_at').defaultTo(knex.fn.now())
