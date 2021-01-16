fa754acacddbf47bc0d77c400121a0ae15dbc1a9
jaolima <jaolima24@gmail.com>
2021-01-16 03:16:50 -0300

    set information in README

--- back-end/migrations/20210116024113_dicipline.js
+++ back-end/migrations/20210116024113_dicipline.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
         table.string('name_dicipline', 50).unique().notNullable()
 
         table.integer('id_subject_niv_1').unsigned()
-        table.foreign('id_subject_niv_1').references('subject_niv_2.id_subject_niv_1')
+        table.foreign('id_subject_niv_1').references('subject_niv_1.id_subject_niv_1')
 
         table.timestamp('created_at').defaultTo(knex.fn.now())
         table.timestamp('updated_at').defaultTo(knex.fn.now())
