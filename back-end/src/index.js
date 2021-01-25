cd211ea17122a71593e8549f621058edd86aeb9d
jaolima <jaolima24@gmail.com>
2021-01-25 20:03:12 -0300

    "

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,14 +1,24 @@
 const fs = require('fs')
 const pdf = require('pdf-parse')
-const pdfFile = fs.readFileSync('src/sample.pdf')
+const pdfFile = fs.readFileSync('src/PDF/TJ MG.pdf')
 
 var methods = {};
 
 methods.timestamp = function() {
+    var string = "a) Casa com a palavra exemplo ",
+        regexp = /a\)/;
+
+    pattern = new RegExp(regexp);
+
+
     pdf(pdfFile).then(function (data){
+        // var AlternativeA = new RegExp(/.*(?= a )/)
+        // var myArray = AlternativeA.exec("aacdbbdbsbz");
+
         console.log(data.numpages)
-        console.log(data.info)
         console.log(data.text)
+        console.log(pattern.exec(string))
+
     })
 };
 
