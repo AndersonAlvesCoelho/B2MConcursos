1df4fb53a41d28643457b80b07b0ae4c36079482
jaolima <jaolima24@gmail.com>
2021-01-26 15:13:47 -0300

    criando formulario para questões

--- front-end/src/pages/RegisterQuestions/index.js
+++ front-end/src/pages/RegisterQuestions/index.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import MenuNavbar from '../../components/MenuNavbar/index';
+import {Col, Container, Row} from "react-bootstrap";
+import {bankData, diciplineData, institutionData, officeData, yearData} from "../../services/filter/dataSelect";
+import { TreeSelect, Input, Radio } from 'antd';
 
 function RegisterQuestions() {
     return (
         <div>
             <MenuNavbar />
-            <h1>Cadastrar questões</h1>
+
+            <Container className="filter-conatiner" >
+                <Row >
+                    <span className="filter-titer mx-1 ml-2">Cadastro de questões:</span>
+                </Row >
+                <Row >
+
+                    <Col className="mt-3" xs={8} md={6}>
+                        <Input
+                            placeholder="Descrição da pergunta.."
+                            allowClear
+                            className="filter-select"
+                            enterButton
+                        />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="mt-3" xs={8} md={6}>
+                        <Input
+                            placeholder="Descrição da pergunta.."
+                            allowClear
+                            className="filter-select"
+                            enterButton
+                        />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="mt-3" xs={8} md={6}>
+                        <Input
+                            placeholder="Descrição da pergunta.."
+                            allowClear
+                            className="filter-select"
+                            enterButton
+                        />
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col className="mt-3" xs={8} md={6}>
+                        <Input
+                            placeholder="Descrição da pergunta.."
+                            allowClear
+                            className="filter-select"
+                            enterButton
+                        />
+                    </Col>
+                </Row>
+
+            </Container>
         </div>
     );
 }
