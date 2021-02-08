6dcc86cc1a06212fe62911ac286f780459145201
jaolima <jaolima24@gmail.com>
2021-02-08 19:15:05 -0300

    migrations

--- back-end/migrations/20210116022907_create_table_subject_niv_1.js
+++ back-end/migrations/20210116022907_create_table_subject_niv_1.js
@@ -1,12 +1,12 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable('subject_niv_1', function(table){
-        table.increments('id_subject_niv_1')
-        table.string('name_subject', 50).unique().notNullable()
-
-        table.integer('id_dicipline').unsigned()
-        table.foreign('id_dicipline').references('dicipline.id_dicipline')
-    })
-};
-
-exports.down = knex => knex.schema.dropTable('subject_niv_1')
+//
+// exports.up = function(knex) {
+//     return knex.schema.createTable('subject_niv_1', function(table){
+//         table.increments('id_subject_niv_1')
+//         table.string('name_subject', 50).unique().notNullable()
+//
+//         table.integer('id_dicipline').unsigned()
+//         table.foreign('id_dicipline').references('dicipline.id_dicipline')
+//     })
+// };
+//
+// exports.down = knex => knex.schema.dropTable('subject_niv_1')
--- back-end/migrations/20210116023729_create_table_subject_niv_7.js
+++ back-end/migrations/20210116023729_create_table_subject_niv_7.js
@@ -1,12 +1,12 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable('subject_niv_7', function(table){
-        table.increments('id_subject_niv_7')
-        table.string('name_subject', 50).unique().notNullable()
-
-        table.integer('id_subject_niv_6').unsigned()
-        table.foreign('id_subject_niv_6').references('subject_niv_6.id_subject_niv_6')
-    })
-};
-
-exports.down = knex => knex.schema.dropTable('subject_niv_7')
+//
+// exports.up = function(knex) {
+//     return knex.schema.createTable('subject_niv_7', function(table){
+//         table.increments('id_subject_niv_7')
+//         table.string('name_subject', 50).unique().notNullable()
+//
+//         table.integer('id_subject_niv_6').unsigned()
+//         table.foreign('id_subject_niv_6').references('subject_niv_6.id_subject_niv_6')
+//     })
+// };
+//
+// exports.down = knex => knex.schema.dropTable('subject_niv_7')
