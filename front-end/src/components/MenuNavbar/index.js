95e12f92998b0b4e32ea086dd741389651d365fb
AndersonAlvesCoelho <a-a-coelho-@hotmail.com>
2021-03-05 21:03:36 -0300

    Ajuste css

--- front-end/src/components/MenuNavbar/index.js
+++ front-end/src/components/MenuNavbar/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
-import { FaFacebook, FaYoutube, FaInstagram, FaTwitter, FaQuestionCircle } from "react-icons/fa";
 
 import logo from '../../assets/img/svg/logo.svg';
 import '../../assets/css/menu-nav-bar.css';
@@ -8,23 +6,25 @@ import '../../assets/css/menu-nav-bar.css';
 function MenuNavbar() {
     return (
         <>
-            <div className="B2M-container">
-                <header className="B2M-header">
-                    <div className="B2M-logo">
+            <div className="B2M-m-container">
+                <header className="B2M-m-header">
+                    <div className="B2M-m-logo">
                         <img src={logo} />
                         <a href="/" >B2MConcursos</a>
                     </div>
                     <nav>
-                        <ul className="B2M-menu">
+                        <ul className="B2M-m-menu">
                             <li><a href="/questoes">Questões </a></li>
                             <li><a href="/cadastrar-questoes">Registrar questão</a></li>
+                            <li className="B2M-m-btn"><a href="/login">Logar</a></li>
+
                         </ul>
                     </nav>
-                    <div className="B2M-login">
-                        <ul className="B2M-menu">
+                    {/* <div className="B2M-m-login">
+                        <ul className="B2M-m-menu">
                             <li><a href="/login">Logar</a></li>
                         </ul>
-                    </div>
+                    </div> */}
                 </header>
             </div>
         </>
--- front-end/src/pages/Questions/index.js
+++ front-end/src/pages/Questions/index.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Jumbotron, Container } from 'react-bootstrap';
-import { Divider } from 'antd';
-import { Menu, Affix, Spin, Empty } from 'antd';
+import { Menu, Spin, Empty } from 'antd';
 import { FaFilter, FaLongArrowAltRight } from "react-icons/fa";
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import '../../assets/css/question.css';
 
-// import { questionData } from '../../services/filter/dataSelect';
 import MenuNavbar from '../../components/MenuNavbar';
 import Alternative from '../../components/Alternative';
 import FilterFixed from '../../components/FilterFixed';
@@ -78,42 +75,34 @@ function Questions(props) {
             {/* Navbar */}
             <MenuNavbar />
 
-            <div className="B2M-q-container">
-                <section className="B2M-guia-page">
-                    <div ><a href="/" >Home</a></div>
-                    <div ><a href="#" >/</a></div>
-                    <div ><a href="/questoes" >Questões</a></div>
-                </section>
-
-
-                <div className="B2M-q-pagination">
-                    <Menu onClick={(e) => setCurrent(e.key)} selectedKeys={[current]} mode="inline" >
-                        <Menu.Item key="mail" onClick={showDrawer} icon={<FaFilter />}>Filtrar</Menu.Item>
-                    </Menu>
-
-                    <FilterFixed visible={visible} onClose={onClose} changerFilter={(e) => { setDataFilter(e); }} />
-                </div>
-            </div>
-
             <section className="B2M-q-question">
 
-                {
-                    question.length !== 0 ? (<>
+
+                {question.length !== 0 ? (<>
+                    <div className="B2M-q-pagination">
                         <Pagination
                             postsPerPage={viewSizeQuestion}
                             totalPosts={qtdQuestion}
                             newPage={onShowSizeChange}
                             paginate={pagerCurrent}
                         />
-                        {question.map((e, x) =>
-                            <div key={x}>
-                                <Alternative data={e} indexQ={x} />
-                            </div>
-                        )}
-                    </>) : (<>
-                        {loadingQuestion ? <div className="center-component"><Spin /></div> : <div className="center-Component"><Empty /></div>}
-                    </>)
-                }
+
+                        <div className="B2M-q-infos">
+                            <FilterFixed visible={visible} onClose={onClose} changerFilter={(e) => { setDataFilter(e); }} />
+                            <button onClick={showDrawer}><FaFilter /></button>
+                        </div>
+
+
+                    </div>
+
+                    {question.map((e, x) =>
+                        <div key={x}>
+                            <Alternative data={e} indexQ={x} />
+                        </div>
+                    )}
+                </>) : (<>
+                    {loadingQuestion ? <div className="center-component"><Spin /></div> : <div className="center-Component"><Empty /></div>}
+                </>)}
 
             </section>
 
