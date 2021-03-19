bfa836e1be94e3190868ead24baff43803695d5d
jaolima <jaolima24@gmail.com>
2021-03-19 19:30:34 -0300

    update 19 03 2021

--- back-end/src/database/seeders/20210211223405-fake-insert-user.js
+++ back-end/src/database/seeders/20210211223405-fake-insert-user.js
@@ -3,13 +3,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('user', [
-      { id_user: 1, email: 'SuperAdministrador', password: '123456', nivel: 1, created_at: new Date(), updated_at: new Date() },
-      { id_user: 2, email: 'Administrador', password: '123456', nivel: 2, created_at: new Date(), updated_at: new Date() },
-      { id_user: 3, email: 'Colaborador', password: '123456', nivel: 3, created_at: new Date(), updated_at: new Date() },
-      { id_user: 4, email: 'Professor', password: '123456', nivel: 4, created_at: new Date(), updated_at: new Date() },
-      { id_user: 5, email: 'Apoiador', password: '123456', nivel: 5, created_at: new Date(), updated_at: new Date() },
-      { id_user: 6, email: 'Aluno', password: '123456', nivel: 6, created_at: new Date(), updated_at: new Date() },
-      { id_user: 7, email: 'Usuario', password: '123456', nivel: 7, created_at: new Date(), updated_at: new Date() },
+      { id_user: 1, nome: 'SuperAdministrador' , email: 'superADM@gmail.com', password: '123456', nivel: 1, created_at: new Date(), updated_at: new Date() },
+      { id_user: 2, nome: 'Administrador' , email: 'ADM@gmail.com', password: '123456', nivel: 2, created_at: new Date(), updated_at: new Date() },
+      { id_user: 3, nome: 'Colaborador' , email: 'colaborador@gmail.com', password: '123456', nivel: 3, created_at: new Date(), updated_at: new Date() },
+      { id_user: 4, nome: 'Professor' , email: 'professor@gmail.com', password: '123456', nivel: 4, created_at: new Date(), updated_at: new Date() },
+      { id_user: 5, nome: 'Apoiador' , email: 'apoiador@gmail.com', password: '123456', nivel: 5, created_at: new Date(), updated_at: new Date() },
+      { id_user: 6, nome: 'Aluno' , email: 'aluno@gmail.com', password: '123456', nivel: 6, created_at: new Date(), updated_at: new Date() },
+      { id_user: 7, nome: 'Usuario' , email: 'usuario@gmail.com', password: '123456', nivel: 7, created_at: new Date(), updated_at: new Date() },
     ], {});
   },
 
