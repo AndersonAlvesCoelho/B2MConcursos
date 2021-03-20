0263d4730022e104bb87c7c512128cb0d6f05d04
AndersonAlvesCoelho <a-a-coelho-@hotmail.com>
2021-03-19 23:43:56 -0300

    fix (Router)

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter , Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PrivateRoute from "./Auth";
-/* // --- ROTAS SEM AUTENTICAÇÃO --- */
-import Home from  './pages';
+// ROTAS SEM AUTENTICAÇÃO
+import Home from './pages';
 import Questions from './pages/Questions';
 import Login from './pages/Login';
-/* // --- ROTAS QUE NECESSITAM DE AUTENTICAÇÃO --- */
+// ROTAS QUE NECESSITAM DE AUTENTICAÇÃO
 import RegisterQuestions from './pages/RegisterQuestions';
 import Perfil from './pages/Dashboard/Perfil';
 
@@ -13,16 +13,15 @@ const Routes = () => (
     <>
         <BrowserRouter>
             <Switch>
-                {/* // --- ROTAS SEM AUTENTICAÇÃO --- */}
+                {/* ROTAS SEM AUTENTICAÇÃO */}
                 <Route path="/" exact component={Home} />
                 <Route path="/questoes" exact component={Questions} />
                 <Route path="/login" exact component={Login} />
 
-                {/* // --- ROTAS QUE NECESSITAM DE AUTENTICAÇÃO --- */}
+                {/* ROTAS QUE NECESSITAM DE AUTENTICAÇÃO */}
                 <PrivateRoute path="/cadastrar-questoes" exact component={RegisterQuestions} />
-                <PrivateRoute path="/dashboard/perfil" exact component={Perfil}
-                   
-                />
+                <PrivateRoute path="/dashboard/perfil" exact component={Perfil} />
+
             </Switch>
         </BrowserRouter>
 
