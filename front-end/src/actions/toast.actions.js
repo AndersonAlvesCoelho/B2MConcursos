c26138eb0f8ab5bdcdeb7794e57b50d4b22d6def
AndersonAlvesCoelho <a-a-coelho-@hotmail.com>
2021-04-03 21:35:59 -0300

    (fix) [merge] corrigir merge

--- front-end/src/actions/toast.actions.js
+++ front-end/src/actions/toast.actions.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { HIDE, SHOW } from '../constants/toast.constants';
-
-export function hide() {
-  return { type: HIDE };
-}
-
-export const show = (status, title, message, delay = 3000) => (dispatch) => {
-  dispatch({
-    type: SHOW,
-    status,
-    title,
-    message,
-    delay,
-  });
-  setTimeout(() => {
-    dispatch(hide());
-  }, delay);
-};
-
-export const showActions = {
-  show,
-=======
 import { HIDE, SHOW } from '../constants';
 
 export function hide() {
@@ -42,5 +19,4 @@ export const show = (status, title, message, delay = 3000) => (dispatch) => {
 
 export const showActions = {
     show,
->>>>>>> 421e7313fad7e23beb563fe272a3b93998336e7a
 };
--- front-end/src/reducers/toast.reducer.js
+++ front-end/src/reducers/toast.reducer.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { HIDE, SHOW } from '../constants/toast.constants';
-
-const initialState = {
-  show: false,
-  status: '',
-  title: '',
-  message: '',
-};
-
-export default function toast(state = initialState, action) {
-  switch (action.type) {
-    case HIDE:
-      return {
-        ...state,
-        show: false,
-        status: '',
-        title: '',
-        message: '',
-        delay: '',
-      };
-    case SHOW:
-      return {
-        ...state,
-        show: true,
-        status: action.status,
-        title: action.title,
-        message: action.message,
-        delay: action.delay,
-      };
-    default:
-      return state;
-  }
-=======
 import { HIDE, SHOW } from '../constants';
 
 const initialState = {
@@ -64,5 +30,4 @@ export default function toast(state = initialState, action) {
         default:
             return state;
     }
->>>>>>> 421e7313fad7e23beb563fe272a3b93998336e7a
 }
