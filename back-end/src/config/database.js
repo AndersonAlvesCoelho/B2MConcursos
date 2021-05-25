5bb616fc25b0ad31220f347baec6a57e6626ee67
jaolima <jaolima24@gmail.com>
2021-05-25 00:10:17 -0300

    gambiarra pra subir o sustema

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -1,30 +1,30 @@
 
 module.exports = {
   // # CONEXÃO EXTERNA B.D
-  // development: {
-  //   dialect: "postgres",
-  //   host: 'localhost',
-  //   database: 'wwb2mc_b2mconcursos',
-  //   username: 'wwb2mc_b2m',
-  //   password: 'LFT#@vD8Q&wB',
-  //   define: {
-  //     timestamps: false,
-  //     underscored: true,
-  //   },
-  //   omitNull: true,
-  // },
-  
-  // # CONEXÃO INTERNA B.D
   development: {
     dialect: "postgres",
     host: 'localhost',
-    database: 'b2mconcursos',
-    username: 'postgres',
-    password: '16798577',
+    database: 'wwb2mc_b2mconcursos',
+    username: 'wwb2mc_b2m',
+    password: 'LFT#@vD8Q&wB',
     define: {
       timestamps: false,
       underscored: true,
     },
     omitNull: true,
   },
+  
+  // # CONEXÃO INTERNA B.D
+  // development: {
+  //   dialect: "postgres",
+  //   host: 'localhost',
+  //   database: 'b2mconcursos',
+  //   username: 'postgres',
+  //   password: '16798577',
+  //   define: {
+  //     timestamps: false,
+  //     underscored: true,
+  //   },
+  //   omitNull: true,
+  // },
 };
